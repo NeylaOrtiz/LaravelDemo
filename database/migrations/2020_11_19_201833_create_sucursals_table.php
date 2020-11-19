ec3170aef4b51ced934ae84127061d0864a13bd7
NeylaOrtz <nlortiz14@misena.edu.co>
2020-11-19 16:05:06 -0500

    Agrego tabla 3

---
+++ database/migrations/2020_11_19_201833_create_sucursals_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSucursalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sucursal', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('Direccion',100);
+            $table->string('Ciudad',100);
+            $table->string('Telefono',100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sucursals');
+    }
+}
---
+++ database/migrations/2020_11_19_203119_create_cuentas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCuentasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cuentas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('Nombre',100);
+            $table->string('tipo de cuenta',100);
+            $table->unsignedBigInteger('id_Sucursal');
+            $table->foreign('id_Sucursal')->references('id')->on('sucursals');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cuentas');
+    }
+}
