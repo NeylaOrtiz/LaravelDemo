61e2c4465d4284846d49572389e0f1c10617f5df
NeylaOrtz <nlortiz14@misena.edu.co>
2020-11-19 08:31:42 -0500

    Agrege tabla 1

---
+++ database/migrations/2020_11_19_114956_create_productos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('Codigo',10);
+            $table->string('Nombre',100);
+            $table->unsignedBigInteger('id_Marca');
+            $table->integer('Precio');
+            $table->integer('Cantidad');
+            $table->timestamps();
+            $table->foreign('id_Marca')->references('id')->on('marcas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
