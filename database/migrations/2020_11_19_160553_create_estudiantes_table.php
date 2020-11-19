0493c69ee3cb9d4db11e874f5e343cb6dba8d3bd
NeylaOrtz <nlortiz14@misena.edu.co>
2020-11-19 11:23:25 -0500

    Agrego tabla 2

---
+++ database/migrations/2020_11_19_160553_create_estudiantes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEstudiantesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estudiantes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('Nombre',100);
+            $table->string('Apellidos',100);
+            $table->string('Acudiente',100);
+            $table->unsignedBigInteger('id_Curso');
+            $table->foreign('id_Curso')->references('id')->on('cursos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estudiantes');
+    }
+}
